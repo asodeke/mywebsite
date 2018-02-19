ea6fa6a37c74115c825a6510efbe3d00e139f455
Ayo Sodeke <35304766+asodeke@users.noreply.github.com>
2018-02-19 12:33:09 -0600

    Add files via upload
    uploading the html, css and js files for website

---
+++ js/script.js
@@ -0,0 +1,26 @@
+function myMap()
+{
+  myCenter=new google.maps.LatLng(39.4899975, -88.2693069);
+  var mapOptions= {
+    center:myCenter,
+    zoom:12, scrollwheel: false, draggable: true,
+    mapTypeId:google.maps.MapTypeId.ROADMAP
+  };
+  var map=new google.maps.Map(document.getElementById("googleMap"),mapOptions);
+
+  var marker = new google.maps.Marker({
+    position: myCenter,
+  });
+  marker.setMap(map);
+}
+
+// Change style of navbar on scroll
+window.onscroll = function() {myFunction()};
+function myFunction() {
+    var navbar = document.getElementById("myNavbar");
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-white";
+    } else {
+        navbar.className = navbar.className.replace(" w3-card w3-animate-top w3-white", "");
+    }
+}
